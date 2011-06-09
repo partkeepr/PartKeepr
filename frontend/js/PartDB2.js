75d5913f7ab40e27a9694d85c2e33dc3cb613bff
Timo A. Hummel <timo@netraver.de>
2011-06-09 03:32:48 +0200

    Fixed jslint warnings

--- frontend/js/PartDB2.js
+++ frontend/js/PartDB2.js
@@ -278,8 +278,8 @@ PartDB2.getMaxUploadSize = function () {
 
 PartDB2.bytesToSize = function (bytes) {
     var sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
-    if (bytes == 0) return 'n/a';
-    var i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)));
+    if (bytes === 0) return 'n/a';
+    var i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)),10);
     return Math.round(bytes / Math.pow(1024, i), 2) + ' ' + sizes[i];
 };
 
