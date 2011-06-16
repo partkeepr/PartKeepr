ff9c500dcb42dbcc11c03b5ed2dc9acdeb3d216d
Felicitus <felicitus@felicitus.org>
2011-06-16 08:33:44 +0800

    Added temporary uploaded file

---
+++ src/de/RaumZeitLabor/PartKeepr/UploadedFile/TempUploadedFile.php
@@ -0,0 +1,12 @@
+<?php
+namespace de\RaumZeitLabor\PartKeepr\UploadedFile;
+declare(encoding = 'UTF-8');
+
+/**
+ * Represents a temporary file. Temporary files are used when
+ * a user uploaded a file, but not attached it to an entity.
+ *
+ * @Entity
+ */
+class TempUploadedFile extends UploadedFile {
+}
