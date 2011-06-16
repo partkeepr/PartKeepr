053a4dd808c2cb40645f07dc28ed276d0115ebe7
Felicitus <felicitus@felicitus.org>
2011-06-16 02:10:45 +0200

    Added "size" information to uploaded files, misc minor changes + documentation

--- src/de/RaumZeitLabor/PartKeepr/Image/CachedImage.php
+++ src/de/RaumZeitLabor/PartKeepr/Image/CachedImage.php
@@ -5,6 +5,10 @@ declare(encoding = 'UTF-8');
 use de\RaumZeitLabor\PartKeepr\PartKeepr;
 
 /**
+ * Represtends a cached image. Cached images are used for scale/resize
+ * operations, so that the resize/scale operation doesn't need to be done
+ * every time a scaled/resized image is requested.
+ * 
  * @Entity 
  */
 class CachedImage {
@@ -42,16 +46,34 @@ class CachedImage {
 	 */
 	private $cacheFile;
 	
+	/**
+	 * Creates a new cache entry for a specific image.
+	 * 
+	 * @param Image $image The image to cache
+	 * @param string $cacheFile The file which holds the cached image
+	 */
 	public function __construct (Image $image, $cacheFile) {
 		$this->originalId = (int)$image->getId();
 		$this->originalType = $image->getType();
 		$this->cacheFile = $cacheFile;
 	}
 	
+	/**
+	 * Returns the cache file
+	 * @return string The cache file including path
+	 */
 	public function getCacheFile () {
 		return $this->cacheFile;
 	}
 	
+	/**
+	 * Removes all caches for a specific image. This is usually called
+	 * when a new version of an image is uploaded.
+	 * 
+	 * Automatically calls "flush" on the entity manager.
+	 * 
+	 * @param Image $image The image to invalidate
+	 */
 	public static function invalidate (Image $image) {
 		$qb = PartKeepr::getEM()->createQueryBuilder();
 		$qb->select(array("c"))
