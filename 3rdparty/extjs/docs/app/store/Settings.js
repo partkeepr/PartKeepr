26a1338d87c5e7ae6c1fe45e2e2a73ba54a0c1aa
Felicitus <felicitus@felicitus.org>
2011-09-06 00:20:44 +0200

    Clean up broken submodules

--- 3rdparty/extjs/docs/app/store/Settings.js
+++
@@ -1,7 +0,0 @@
-/**
- * Store for keeping Docs app settings
- */
-Ext.define('Docs.store.Settings', {
-    extend: 'Ext.data.Store',
-    model: 'Docs.model.Setting'
-});
--- 3rdparty/extjs/docs/guides/DOM/README.js
+++
@@ -1,3 +0,0 @@
-Ext.data.JsonP.DOM({
-  "guide": "<h1>The DOM</h1>\n\n<hr />\n"
-});
