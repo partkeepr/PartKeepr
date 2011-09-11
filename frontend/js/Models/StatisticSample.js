39d6570dafb3e78b23340428afd19c19feed4f9c
Felicitus <felicitus@felicitus.org>
2011-09-11 14:09:28 +0200

    Bugfix: Specify date format explicitely because only chrome seems to have auto-detect for Y-m-d H:i:s

--- frontend/js/Models/StatisticSample.js
+++ frontend/js/Models/StatisticSample.js
@@ -1,7 +1,7 @@
 Ext.define("PartKeepr.StatisticSample", {
 	extend: "Ext.data.Model",
 	fields: [
-	         {	name: 'start',	type: 'date'},
+	         {	name: 'start',	type: 'date', dateFormat: 'Y-m-d H:i:s'},
 	         {	name: 'parts',	type: 'int', useNull: true },
 	         {	name: 'categories',	type: 'int', useNull: true }
 	         ]
