a747389e9620894218b2a2e29fb6882f4d75191c
Felicitus <felicitus@felicitus.org>
2011-12-22 06:47:09 +0100

    Added some documentation to the part manufacturer

--- src/backend/de/RaumZeitLabor/PartKeepr/Part/PartManufacturer.php
+++ src/backend/de/RaumZeitLabor/PartKeepr/Part/PartManufacturer.php
@@ -30,26 +30,48 @@ class PartManufacturer extends BaseEntity implements Serializable, Deserializabl
 	 */
 	private $partNumber;
 	
+	/**
+	 * Sets the part which belongs to this manufacturer entry
+	 * @param Part $part
+	 */
 	public function setPart (Part $part) {
 		$this->part = $part;
 	}
 	
+	/**
+	 * Returns the part which belongs to this manufacturer entry
+	 */
 	public function getPart () {
 		return $this->part;
 	}
 	
+	/**
+	 * Sets the manufacturer which belongs to this entry
+	 * @param Manufacturer $manufacturer
+	 */
 	public function setManufacturer (Manufacturer $manufacturer) {
 		$this->manufacturer = $manufacturer;
 	}
 	
+	/**
+	 * Returns the manufacturer which belongs to this part
+	 */
 	public function getManufacturer () {
 		return $this->manufacturer;
 	}
 	
+	/**
+	 * Sets the manufacturer-specific part number
+	 * @param string $partNumber
+	 */
 	public function setPartNumber ($partNumber) {
 		$this->partNumber = $partNumber;
 	}
 	
+	/**
+	 * Returns the manufacturer-specific part number
+	 * @return string The part number
+	 */
 	public function getPartNumber () {
 		return $this->partNumber;
 	}
