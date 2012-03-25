e16c50750c9909ea23a2406e60f8c915d4303eaa
Felicitus <felicitus@felicitus.org>
2012-03-25 03:35:36 +0200

    Use a sqlite-db for testing purposes

--- config-test.php
+++ config-test.php
@@ -3,10 +3,5 @@ namespace de\RaumZeitLabor\PartKeepr;
 
 use de\RaumZeitLabor\PartKeepr\Util\Configuration;
 
-Configuration::setOption("partkeepr.database.username", "partkeepr");
-Configuration::setOption("partkeepr.database.password", "partkeepr");
-Configuration::setOption("partkeepr.database.hostname", "localhost");
 Configuration::setOption("partkeepr.database.dbname", "partkeepr-test");
-Configuration::setOption("partkeepr.database.driver", "pdo_mysql");
-
-?>
+Configuration::setOption("partkeepr.database.driver", "pdo_sqlite");
