04ddecabe4c4f75d099e9aea3f58ea23b6c6fae3
Peter Mousley <scrupeus@mousley.com.au>
2012-05-13 16:55:55 +1000

    Added user preference for formatting decimal precision of prices, issue #154

---
+++ src/frontend/js/Components/User/Preferences/FormattingPreferences.js
@@ -0,0 +1,29 @@
+Ext.define('PartKeepr.FormattingPreferencesPanel', {
+	extend: 'Ext.form.FormPanel',
+	title: i18n("Formatting"),
+	bodyStyle: 'background:#DBDBDB;padding: 10px;',
+	initComponent: function () {
+		this.priceNumDecimalsField = Ext.create("Ext.form.field.Number", {
+			name: 'priceNumDecimalsField',
+			fieldLabel: i18n('Decimal precision'),
+			labelWidth: 150,
+			columnWidth: 0.5,
+			minValue: 0,
+			maxValue: 4,
+			allowDecimals: false,
+			listeners: {
+				change: function(field, newValue) {
+					PartKeepr.getApplication().setUserPreference("partkeepr.formatting.price.numdecimals", newValue);
+				}
+			}
+		});
+
+		var numDecimals = PartKeepr.getApplication().getUserPreference("partkeepr.formatting.price.numdecimals", 2);
+		this.priceNumDecimalsField.setValue(numDecimals);
+
+		this.items = [ this.priceNumDecimalsField ];
+
+		this.callParent();
+	},
+});
+
--- src/frontend/js/Components/User/UserPreferences.js
+++ src/frontend/js/Components/User/UserPreferences.js
@@ -7,9 +7,10 @@ Ext.define('PartKeepr.UserPreferencePanel', {
 		
 		this.passwordChangePanel = Ext.create("PartKeepr.UserPasswordChangePanel");
 		this.tipsPanel = Ext.create("PartKeepr.TipOfTheDayPreferencesPanel");
+		this.formattingPanel = Ext.create("PartKeepr.FormattingPreferencesPanel");
 		this.categoryTreePanel = Ext.create("PartKeepr.CategoryTreePreferencesPanel");
-        this.stockPanel = Ext.create("PartKeepr.StockPreferencesPanel");
-		this.items = [ this.tipsPanel, this.categoryTreePanel, this.passwordChangePanel, this.stockPanel ];
+		this.stockPanel = Ext.create("PartKeepr.StockPreferencesPanel");
+		this.items = [ this.tipsPanel, this.formattingPanel, this.categoryTreePanel, this.passwordChangePanel, this.stockPanel ];
 		this.callParent();
 	}
 });
