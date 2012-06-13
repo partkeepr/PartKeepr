fec5d3947ee5dcf7b2a9ae8f3ad51015831e9295
Felicitus <felicitus@felicitus.org>
2012-06-13 16:59:50 +0200

    Preparations for migration to extjs 4.1

--- src/frontend/js/ExtJS/Enhancements/Ext.data.reader.Json-exceptionHandling.js
+++ src/frontend/js/ExtJS/Enhancements/Ext.data.reader.Json-exceptionHandling.js
@@ -3,20 +3,31 @@ Ext.override(Ext.data.reader.Json, {
         var data;
         try {
             data = Ext.decode(response.responseText);
+            return this.readRecords(data);
         }
         catch (ex) {
+            error = new Ext.data.ResultSet({
+                total  : 0,
+                count  : 0,
+                records: [],
+                success: false,
+                message: ex.message
+            });
+
+            this.fireEvent('exception', this, response, error);
+
         	var exception = {
         			message: i18n("Critical Error"),
         			detail: i18n("The server returned a response which we were not able to interpret.")
         	};
-        	
+
         	var request = {
         			response: response.responseText
         	};
-        	
+
         	PartKeepr.ExceptionWindow.showException(exception, request);
+            return error;
         }
 
-        return data;
     }
 });
