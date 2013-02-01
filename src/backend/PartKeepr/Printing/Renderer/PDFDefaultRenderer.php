ddaf71cb8ec5a1949ec237aa93c44ecca6441d79
Sven Steckmann <spm_gitcommit@steckmann.de>
2013-02-01 22:47:46 +0100

    Cleanup PDFDefaultRenderer and make the methods to render object independent. Now the renderer is flexible enough to use the configuration to describe the label for many common cases. Additionally adds 1D barcodes to be used with parts too.

--- src/backend/PartKeepr/Printing/Renderer/PDFDefaultRenderer.php
+++ src/backend/PartKeepr/Printing/Renderer/PDFDefaultRenderer.php
@@ -25,7 +25,7 @@ class PDFDefaultRenderer extends TCPDFAbstractRenderer{
 	private $defaultConfiguration = array( 
 			'barcodeEnable' => false,
 			'barcodeType' => 'QRCODE,L',
-			'textBarcode' => "PA/!!id!!",
+			'textBarcode' => "!!barcodeId,%06d!!",
 			'barcodeWidth' => "12",
 			'barcodeHeight' => "12",
 			'barcodeXPos' => "-7",
@@ -35,7 +35,9 @@ class PDFDefaultRenderer extends TCPDFAbstractRenderer{
 			'fontFamily' => 'times',
 			'fontStyle' => '',
 			'fontSize' => 12,
-			'text' => '<span style="font-size: 11pt;"><b>!!name!!</b></span><br><span style="font-size: 8pt;">!!description!!</span>'
+			'text' => '<span style="font-size: 11pt;"><b>!!name!!</b></span><br><span style="font-size: 8pt;">!!description!!</span>',
+			'textXOffset' => 0,
+			'textYOffset' => 0
 			);
 	
 	/**
@@ -58,14 +60,11 @@ class PDFDefaultRenderer extends TCPDFAbstractRenderer{
 		if ( is_array( $data ) ){
 			if (count($data)>0){
 				$elem = reset($data);
-				if ($elem instanceof StorageLocation){
-					$this->renderStorageLocations($data);
-				} elseif ($elem instanceof Part){
-					$this->renderParts($data);
-				}
-				else{
+				if ($elem instanceof StorageLocation || $elem instanceof Part ){
+					$this->renderObjects($data);
+				}else{
 					throw new RendererNotFoundException("Unable to handle object type with renderer PDFLabelRenderer.",
-							get_class($elem),array("StorageLocation"));
+							get_class($elem),array("StorageLocation","Part"));
 				}
 			}
 		}
@@ -75,84 +74,14 @@ class PDFDefaultRenderer extends TCPDFAbstractRenderer{
 			passRenderingData( array( $ata ) );
 		}
 	}
-	
-	/**
-	 * This method renders an array of StorageLocations to our sheet.
-	 * @param array $locations
-	 */
-	private function renderStorageLocations( array $locations ){
-		foreach ($locations as $location){
-			$this->renderCellFromLocation( $location );
-		}
-	}
-	
-	/**
-	 * This method just renders the next single cell with the given location 
-	 * as content.
-	 */
-	private function renderCellFromLocation( StorageLocation $location ){
-		// Move the pointer to the next cell and initialize PDF class
-		// correctly. Also draws the grid if necessary.
-		$this->initNextCell();
-
-		$padding = 3;
-
-		$name = $location->getName();
-
-		$this->pdf->SetCellPadding($padding);
-		$this->pdf->SetFont( $this->configuration['fontFamily'], 
-				$this->configuration['fontStyle'], 
-				$this->configuration['fontSize']);
-		$this->pdf->SetXY( $this->xCellPos,$this->yCellPos );
-
-		// Start clipping. Every thing with StartTransform and StopTransform
-		// will be clipped to the Rect. This is a cool feature if one field is
-		// too long, it will not destroy the rest of your page and it is partial
-		// usable.
-		$this->pdf->StartTransform();		
-		// Draw clipping rectangle to match html cell.
-		$this->pdf->Rect($this->xCellPos, $this->yCellPos, $this->layout->getCellWidthInMM(),
-				$this->layout->getCellHeightInMM(), 'CNZ');		
-
-		$this->pdf->MultiCell( $this->layout->getCellWidthInMM(),
-				$this->layout->getCellHeightInMM(), $name, '0');		
-
-		$this->pdf->StopTransform();
-
-		// Place a Barcode at the end
-		if ($this->configuration['barcodeEnable']){
-			$barcodeWidth = $this->configuration['barcodeWidth'];
-			$barcodeHeight = $this->layout->getCellHeightInMM() - 5;
-			
-			if ($this->configuration['barcode2D']){
-				$this->pdf->write2DBarcode($name, $this->configuration['barcodeType'], 
-						$this->xCellPos + $this->layout->getCellWidthInMM() - $barcodeWidth - 5,
-						$this->yCellPos + $this->layout->getCellHeightInMM() / 2 - $barcodeHeight / 2,
-						$barcodeWidth,
-						$barcodeHeight
-						);
-			}
-			else {				
-				$style = array('text'=> $this->configuration['barcodeWithText'] );
-				$this->pdf->write1DBarcode($name, $this->configuration['barcodeType'], 
-						$this->xCellPos + $this->layout->getCellWidthInMM() - $barcodeWidth - 5,
-						$this->yCellPos + $this->layout->getCellHeightInMM() / 2 - $barcodeHeight / 2,
-						$barcodeWidth,
-						$barcodeHeight,
-						"",
-						$style
-						);
-			}
-		}
-	}
-	
+		
 	/**
 	 * This method renders an array of Parts to our sheet.
 	 * @param array $parts
 	 */
-	private function renderParts( array $parts ){
-		foreach ($parts as $part){
-			$this->renderCellFromPart( $part );
+	private function renderObjects( array $objects ){
+		foreach ($objects as $obj){
+			$this->renderCell( $obj );
 		}
 	}
 	
@@ -160,14 +89,14 @@ class PDFDefaultRenderer extends TCPDFAbstractRenderer{
 	 * This method just renders the next single cell with a part
 	 * as content.
 	 */
-	private function renderCellFromPart( Part $part ){
+	private function renderCell( $object ){
 		// Move the pointer to the next cell and initialize PDF class
 		// correctly. Also draws the grid if necessary.
 		$this->initNextCell();
 	
 		$padding = 3;
 		
-		$dataReplacement = new Placeholder( $part, "!!", "!!" );
+		$dataReplacement = new Placeholder( $object, "!!", "!!" );
 		
 		$text = $dataReplacement->apply($this->configuration['text']);
 		$barcodeId = $dataReplacement->apply($this->configuration['textBarcode'] );
@@ -191,6 +120,7 @@ class PDFDefaultRenderer extends TCPDFAbstractRenderer{
 			
 			$regions = array();
 			
+			// Shitty workaround for a Bug in TCPDF the ower is not interested in fixing it!
 			$this->pdf->resetInternalMargins();
 	
 			if ($this->configuration['barcode2D']){
@@ -199,7 +129,17 @@ class PDFDefaultRenderer extends TCPDFAbstractRenderer{
 						$this->yCellPos + $yPos - $height / 2,
 						$width,
 						$height
-				);
+					);
+			} else {				
+				$style = array('text'=> $this->configuration['barcodeWithText'] );
+				$this->pdf->write1DBarcode($barcodeId, $this->configuration['barcodeType'], 
+						$this->xCellPos + $xPos - $width / 2 ,
+						$this->yCellPos + $yPos - $height / 2,
+						$width,
+						$height,
+						"",
+						$style
+						);
 			}
 			
 			// This region will add a "do not write text here" over our barcode This is a kind
@@ -258,10 +198,13 @@ class PDFDefaultRenderer extends TCPDFAbstractRenderer{
 		$this->pdf->Rect($this->xCellPos, $this->yCellPos, $this->layout->getCellWidthInMM(),
 				$this->layout->getCellHeightInMM(), 'CNZ');
 	
+		$xPosTextOffsetParameter = new PercentOrNumericHelper($this->configuration['textXOffset']);
+		$yPosTextOffsetParameter = new PercentOrNumericHelper($this->configuration['textYOffset']);
+
 		$this->pdf->WriteHTMLCell( $this->layout->getCellWidthInMM(),
 				$this->layout->getCellHeightInMM(), 
-				$this->xCellPos, 
-				$this->yCellPos,
+				$this->xCellPos + $xPosTextOffsetParameter->getValueUnbound( $this->layout->getCellWidthInMM() ), 
+				$this->yCellPos + $yPosTextOffsetParameter->getValueUnbound( $this->layout->getCellWidthInMM() ),
 				$text);
 	
 		$this->pdf->StopTransform();
