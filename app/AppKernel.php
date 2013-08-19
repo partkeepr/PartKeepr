c3891031907436bfd305db3bbf2ae18fd83bd254
Felicitus <felicitus@felicitus.org>
2013-08-19 17:43:02 +0200

    Added initial index controller

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,8 @@ class AppKernel extends Kernel
             new FOS\RestBundle\FOSRestBundle(),
             new JMS\SerializerBundle\JMSSerializerBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+
+            new PartKeepr\FrontendBundle\PartKeeprFrontendBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
