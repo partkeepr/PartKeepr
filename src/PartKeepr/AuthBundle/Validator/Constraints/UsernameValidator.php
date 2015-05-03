b5a1e5716c4341511617dad16a8fbe67972a79d9
Felicitus <felicitus@felicitus.org>
2015-05-03 19:56:49 +0200

    Escape dash (-) to have the regular expression work correctly

--- src/PartKeepr/AuthBundle/Validator/Constraints/UsernameValidator.php
+++ src/PartKeepr/AuthBundle/Validator/Constraints/UsernameValidator.php
@@ -8,7 +8,7 @@ class UsernameValidator extends ConstraintValidator
 {
     public function validate($value, Constraint $constraint)
     {
-        if (!preg_match('/^[a-zA-Za0-9.-_\/\\\]{3,50}$/', $value, $matches)) {
+        if (!preg_match('/^[a-zA-Za0-9.\-_\/\\\]{3,50}$/', $value, $matches)) {
             // If you're using the new 2.5 validation API (you probably are!)
             $this->context->buildViolation($constraint->message)
                 ->setParameter('%string%', $value)
