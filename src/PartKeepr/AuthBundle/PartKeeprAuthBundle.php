b47194deed153c8975513c9e941cd1d4d331804e
Timo A. Hummel <felicitus@felicitus.org>
2015-05-23 20:43:34 +0200

    Added command "partkeepr:user:create"

--- src/PartKeepr/AuthBundle/PartKeeprAuthBundle.php
+++ src/PartKeepr/AuthBundle/PartKeeprAuthBundle.php
@@ -2,8 +2,17 @@
 
 namespace PartKeepr\AuthBundle;
 
+use PartKeepr\AuthBundle\Console\Command\CreateUserCommand;
+use Symfony\Component\Console\Application;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class PartKeeprAuthBundle extends Bundle
 {
+    /**
+     * {@inheritDoc}
+     */
+    public function registerCommands(Application $application)
+    {
+        $application->add(new CreateUserCommand());
+    }
 }
