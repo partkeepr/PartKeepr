2d7a112110119901abec0eada43dd5bc6488eec9
Felicitus <felicitus@felicitus.org>
2015-05-26 18:02:24 +0200

    This bundle extends the NelmioApiDocBundle so that it can read annotations of the parent class. That way, PartKeepr can have one superclass with commonly used parameters (like start and limit for pagination) without the need of re-defining common parameters for each child class.
    The current limitation is that it currently only works for direct descendants and not for the whole class hierarchy.

---
+++ src/PartKeepr/RESTBundle/Request/ParamReader.php
@@ -0,0 +1,24 @@
+<?php
+namespace PartKeepr\RESTBundle\Request;
+
+use FOS\RestBundle\Request\ParamReader as FOSRestParamReader;
+
+class ParamReader extends FOSRestParamReader
+{
+    public function getParamsFromMethod(\ReflectionMethod $method)
+    {
+        $parentParams = array();
+        $params = parent::getParamsFromMethod($method);
+
+        // This loads the annotations of the parent method
+        $declaringClass = $method->getDeclaringClass();
+        $parentClass = $declaringClass->getParentClass();
+
+        if ($parentClass && $parentClass->hasMethod($method->getShortName())) {
+            $parentMethod = $parentClass->getMethod($method->getShortName());
+            $parentParams = parent::getParamsFromMethod($parentMethod);
+        }
+
+        return array_merge($params, $parentParams);
+    }
+}
