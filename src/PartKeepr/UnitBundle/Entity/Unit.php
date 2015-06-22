9762f9f74344134ea7ddd74fc26eeacae5a0a747
Felicitus <felicitus@felicitus.org>
2015-06-22 20:22:12 +0200

    Added validators for the Unit Entity, removed obsolete code

--- src/PartKeepr/UnitBundle/Entity/Unit.php
+++ src/PartKeepr/UnitBundle/Entity/Unit.php
@@ -2,121 +2,123 @@
 namespace PartKeepr\UnitBundle\Entity;
 
 use Doctrine\Common\Collections\ArrayCollection;
-use	PartKeepr\Util\BaseEntity,
-	PartKeepr\SiPrefixBundle\Entity\SiPrefix,
-    Doctrine\ORM\Mapping as ORM,
-	PartKeepr\DoctrineReflectionBundle\Annotation\TargetService;
+use Doctrine\ORM\Mapping as ORM;
+use PartKeepr\DoctrineReflectionBundle\Annotation\TargetService;
+use PartKeepr\SiPrefixBundle\Entity\SiPrefix;
+use PartKeepr\Util\BaseEntity;
 use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
- * This object represents an unit. Units can be: Volt, Hertz etc. 
- *  
+ * This object represents an unit. Units can be: Volt, Hertz etc.
+ *
  * @ORM\Entity
  * @TargetService(uri="/api/units")
  **/
-class Unit {
-	/**
-	 * @ORM\Id @ORM\Column(type="integer")
-	 * @ORM\GeneratedValue(strategy="AUTO")
-	 * @Groups({"default"})
-	 * @var integer
-	 */
-	private $id;
+class Unit extends BaseEntity
+{
+    /**
+     * The name of the unit (e.g. Volts, Ampere, Farad, Metres)
+     * @ORM\Column(type="string")
+     * @Groups({"default"})
+     * @Assert\Type(type="string")
+     * @Assert\NotBlank(message="unit.name.not_blank")
+     *
+     * @var string
+     */
+    private $name;
 
-	/**
-	 * Returns the ID of this object.
-	 *
-	 * @param none
-	 *
-	 * @return int The ID of this object
-	 */
-	public function getId()
-	{
-		return $this->id;
-	}
+    /**
+     * The symbol of the unit (e.g. V, A, F, m)
+     * @ORM\Column(type="string")
+     * @Groups({"default"})
+     * @Assert\Type(type="string")
+     * @Assert\NotBlank(message="unit.symbol.not_blank")
+     *
+     * @var string
+     */
+    private $symbol;
 
-	public function setId ($id) {
-		$this->id = $id;
-	}
-	/**
-	 * The name of the unit (e.g. Volts, Ampere, Farad, Metres)
-	 * @ORM\Column(type="string")
-	 *
-	 * @var string
-	 * @Groups({"default"})
-	 */
-	private $name;
-	
-	/**
-	 * The symbol of the unit (e.g. V, A, F, m)
-	 * @ORM\Column(type="string")
-	 * @var string
-	 * @Groups({"default"})
-	 */
-	private $symbol;
-	
-	/**
-	 * Defines the allowed SiPrefixes for this parameter unit
-	 * @ORM\ManyToMany(targetEntity="PartKeepr\SiPrefixBundle\Entity\SiPrefix")
-	 * @ORM\JoinTable(name="UnitSiPrefixes",
-	 * 			joinColumns={@ORM\JoinColumn(name="unit_id", referencedColumnName="id")},
-	 * 			inverseJoinColumns={@ORM\JoinColumn(name="siprefix_id", referencedColumnName="id")}
-	 * 			)
-	 * @Groups({"default"})
-	 *
-	 * @var ArrayCollection
-	 */
-	public $prefixes;
-	
-	/**
-	 * Creates a new Unit.
-	 */
-	public function __construct () {
-		$this->prefixes = new \Doctrine\Common\Collections\ArrayCollection();
-	}
-	
-	/**
-	 * Sets the name for this unit
-	 * @param string $name the name for this unit
-	 */
-	public function setName ($name) {
-		$this->name = $name;
-	}
-	
-	/**
-	 * Returns the name for this unit
-	 * @return string The unit name
-	 */
-	public function getName () {
-		return $this->name;
-	}
-	
-	/**
-	 * Sets the symbol for this unit
-	 * @param string $symbol The symbol
-	 */
-	public function setSymbol ($symbol) {
-		$this->symbol = $symbol;
-	}
-	
-	/**
-	 * Returns the symbol for this unit
-	 * @return string The symbol
-	 */
-	public function getSymbol () {
-		return $this->symbol;
-	}
+    /**
+     * Defines the allowed SiPrefixes for this parameter unit
+     * @ORM\ManyToMany(targetEntity="PartKeepr\SiPrefixBundle\Entity\SiPrefix")
+     * @ORM\JoinTable(name="UnitSiPrefixes",
+     *            joinColumns={@ORM\JoinColumn(name="unit_id", referencedColumnName="id")},
+     *            inverseJoinColumns={@ORM\JoinColumn(name="siprefix_id", referencedColumnName="id")}
+     *            )
+     * @Groups({"default"})
+     * @Assert\All({
+     *      @Assert\Type(type="PartKeepr\SiPrefixBundle\Entity\SiPrefix")
+     * })
+     * @var ArrayCollection
+     */
+    private $prefixes;
 
-	/**
-	 * Returns the si-prefix list for this unit
-	 * @return array An array of SiPrefix objects
-	 */
-	public function getPrefixes () {
-		return $this->prefixes;
-	}
+    /**
+     * Creates a new Unit.
+     */
+    public function __construct()
+    {
+        $this->prefixes = new ArrayCollection();
+    }
 
-	public function setPrefixes ($array) {
-		var_dump($array);
-		$this->prefixes = $array;
-	}
+    /**
+     * Sets the name for this unit
+     *
+     * @param string $name the name for this unit
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * Returns the name for this unit
+     *
+     * @return string The unit name
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Sets the symbol for this unit
+     *
+     * @param string $symbol The symbol
+     */
+    public function setSymbol($symbol)
+    {
+        $this->symbol = $symbol;
+    }
+
+    /**
+     * Returns the symbol for this unit
+     *
+     * @return string The symbol
+     */
+    public function getSymbol()
+    {
+        return $this->symbol;
+    }
+
+    /**
+     * Returns the si-prefix list for this unit
+     *
+     * @return array An array of SiPrefix objects
+     */
+    public function getPrefixes()
+    {
+        return $this->prefixes;
+    }
+
+    /**
+     * Sets the SiPrefixes
+     *
+     * @param $array
+     */
+    public function setPrefixes($array)
+    {
+        $this->prefixes = $array;
+    }
 }
---
+++ src/PartKeepr/UnitBundle/Tests/Model/UnitTest.php
@@ -0,0 +1,40 @@
+<?php
+namespace PartKeepr\UnitBundle\Tests\Model;
+
+use PartKeepr\SiPrefixBundle\Entity\SiPrefix;
+use PartKeepr\UnitBundle\Entity\Unit;
+
+class UnitTest extends \PHPUnit_Framework_TestCase
+{
+
+    public function testName()
+    {
+        $unit = $this->getUnit();
+
+        $unit->setName("Volt");
+        $this->assertEquals("Volt", $unit->getName());
+    }
+
+    public function testSymbol()
+    {
+        $unit = $this->getUnit();
+
+        $unit->setSymbol("V");
+
+        $this->assertEquals("V", $unit->getSymbol());
+    }
+
+    public function testPrefixes()
+    {
+        $unit = $this->getUnit();
+        $newSiPrefix = new SiPrefix();
+
+        $unit->setPrefixes(array($newSiPrefix));
+        $this->assertEquals(array($newSiPrefix), $unit->getPrefixes());
+    }
+
+    private function getUnit()
+    {
+        return new Unit();
+    }
+}
