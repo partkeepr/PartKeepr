27658361b8a18cbdd885edf31a29c7e998cb43ce
Felicitus <felicitus@felicitus.org>
2015-07-07 17:12:57 +0200

    Added configuration to allow extjs models to be auto-generated per environment

--- src/PartKeepr/DoctrineReflectionBundle/CacheWarmer/ModelCacheWarmer.php
+++ src/PartKeepr/DoctrineReflectionBundle/CacheWarmer/ModelCacheWarmer.php
@@ -3,6 +3,7 @@ namespace PartKeepr\DoctrineReflectionBundle\CacheWarmer;
 
 
 use Doctrine\Common\Persistence\ManagerRegistry;
+use Symfony\Component\DependencyInjection\Container;
 use PartKeepr\DoctrineReflectionBundle\Services\ReflectionService;
 use Symfony\Component\HttpKernel\CacheWarmer\CacheWarmer;
 
@@ -10,16 +11,18 @@ class ModelCacheWarmer extends CacheWarmer{
 
     private $registry;
     private $reflectionService;
+    private $container;
 
     /**
      * Constructor.
      *
      * @param ManagerRegistry $registry A ManagerRegistry instance
      */
-    public function __construct(ManagerRegistry $registry, ReflectionService $reflectionService)
+    public function __construct(ManagerRegistry $registry, ReflectionService $reflectionService, Container $container)
     {
         $this->reflectionService = $reflectionService;
         $this->registry = $registry;
+        $this->container = $container;
     }
 
     /**
@@ -37,14 +40,19 @@ class ModelCacheWarmer extends CacheWarmer{
      */
     public function warmUp($cacheDir)
     {
-        $cacheDir .= "/doctrinereflection";
+        if ($this->container->getParameter("partkeepr.auto_generate_extjs_models")) {
+            echo "FOO";
+            $cacheDir .= "/doctrinereflection";
 
-        $entities = $this->reflectionService->getEntities();
+            $entities = $this->reflectionService->getEntities();
 
-        foreach ($entities as $entity) {
-            $model = $this->reflectionService->getEntity($entity);
-            @mkdir($cacheDir, 0777, true);
-            $this->writeCacheFile($cacheDir."/".$entity.'.js', $model);
+            foreach ($entities as $entity) {
+                $model = $this->reflectionService->getEntity($entity);
+
+                @mkdir($cacheDir, 0777, true);
+                $this->writeCacheFile($cacheDir."/".$entity.'.js', $model);
+            }
         }
+
     }
 }
