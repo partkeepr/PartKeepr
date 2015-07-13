aeebf552009032f532a5eb22dc25d9933ff03fa9
Felicitus <felicitus@felicitus.org>
2015-07-13 23:48:22 +0200

    Removed obsolete manager/service code

--- src/PartKeepr/FootprintBundle/Entity/FootprintCategoryService.php
+++
@@ -1,9 +0,0 @@
-<?php 
-namespace PartKeepr\FootprintBundle\Entity;
-
-use PartKeepr\Category\AbstractCategoryService;
-
-class FootprintCategoryService extends AbstractCategoryService {
-	protected $categoryManagerClass = "PartKeepr\FootprintBundle\Entity\FootprintCategoryManager";
-	protected $categoryClass = "PartKeepr\FootprintBundle\Entity\FootprintCategory";
-}
--- src/PartKeepr/PartBundle/Entity/PartCategoryService.php
+++
@@ -1,9 +0,0 @@
-<?php 
-namespace PartKeepr\PartBundle\Entity;
-
-use PartKeepr\Category\AbstractCategoryService;
-
-class PartCategoryService extends AbstractCategoryService {
-	protected $categoryManagerClass = "PartKeepr\PartBundle\Entity\PartCategoryManager";
-	protected $categoryClass = "PartKeepr\PartBundle\Entity\PartCategory";
-}
