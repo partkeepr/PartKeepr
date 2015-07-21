f39ffce04eba9bd04c39327c0f8c2fd46dac4985
Felicitus <felicitus@felicitus.org>
2015-07-22 00:00:48 +0200

    Implemented getImage for the footprint image

---
+++ src/PartKeepr/FootprintBundle/Controller/FootprintImageController.php
@@ -0,0 +1,15 @@
+<?php
+namespace PartKeepr\FootprintBundle\Controller;
+
+use PartKeepr\ImageBundle\Controller\ImageController;
+
+class FootprintImageController extends ImageController
+{
+    /**
+     * @inheritdoc
+     */
+    protected function getEntityClass()
+    {
+        return "PartKeepr\\FootprintBundle\\Entity\\FootprintImage";
+    }
+}
