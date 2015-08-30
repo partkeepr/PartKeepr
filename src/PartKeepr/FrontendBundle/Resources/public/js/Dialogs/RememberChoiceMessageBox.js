79f1d1fe04b69523a393d349effdbd3c2213cfb8
Felicitus <felicitus@felicitus.org>
2015-08-30 16:52:21 +0200

    Fixed layout error issue for the remember choice message box

--- src/PartKeepr/FrontendBundle/Resources/public/js/Dialogs/RememberChoiceMessageBox.js
+++ src/PartKeepr/FrontendBundle/Resources/public/js/Dialogs/RememberChoiceMessageBox.js
@@ -1,36 +1,48 @@
 Ext.define('PartKeepr.RememberChoiceMessageBox', {
     extend: 'Ext.window.MessageBox',
-   
-    escButtonAction: null,
-    
-    initComponent: function () {
-    	this.callParent();
-    	
-    	this.rememberChoiceCheckbox = Ext.create("Ext.form.field.Checkbox", {
-    		margin: {
-    			top: "10px"
-    		},
-    		boxLabel: i18n("Don't ask again")
-    	});
-    	
-    	this.promptContainer.add(this.rememberChoiceCheckbox);
-    },
-    onEsc: function() {
-    	
-    	if (this.escButtonAction !== null) {
-    		var btnIdx;
-    		
-    		switch (this.escButtonAction) {
-    			case "ok": btnIdx = 0; break;
-    			case "yes": btnIdx = 1; break;
-    			case "no": btnIdx = 2; break;
-    			case "cancel": btnIdx = 3; break; 
-    			default: btnIdx = 3; break;
-    		}
 
-    		this.btnCallback(this.msgButtons[btnIdx]);
-    	} else {
-    		this.callParent();
-    	}
+    escButtonAction: null,
+
+    initComponent: function ()
+    {
+        this.callParent();
+
+        this.rememberChoiceCheckbox = Ext.create("Ext.form.field.Checkbox", {
+            boxLabel: i18n("Don't ask again"),
+            margin: {
+                top: 10
+            }
+        });
+
+        this.promptContainer.add(this.rememberChoiceCheckbox);
+
+    },
+    onEsc: function ()
+    {
+        if (this.escButtonAction !== null) {
+            var btnIdx;
+
+            switch (this.escButtonAction) {
+                case "ok":
+                    btnIdx = 0;
+                    break;
+                case "yes":
+                    btnIdx = 1;
+                    break;
+                case "no":
+                    btnIdx = 2;
+                    break;
+                case "cancel":
+                    btnIdx = 3;
+                    break;
+                default:
+                    btnIdx = 3;
+                    break;
+            }
+
+            this.btnCallback(this.msgButtons[btnIdx]);
+        } else {
+            this.callParent();
+        }
     }
 });
