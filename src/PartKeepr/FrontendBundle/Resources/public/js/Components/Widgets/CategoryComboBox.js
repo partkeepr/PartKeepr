953abc9a6c4cb606559f4cf104c319e00ee2cb1a
Felicitus <felicitus@felicitus.org>
2015-08-30 16:25:16 +0200

    Re-added reload button for categories

--- src/PartKeepr/FrontendBundle/Resources/public/js/Components/Widgets/CategoryComboBox.js
+++ src/PartKeepr/FrontendBundle/Resources/public/js/Components/Widgets/CategoryComboBox.js
@@ -1,4 +1,36 @@
 Ext.define("PartKeepr.CategoryComboBox",{
     extend:"Ext.ux.TreePicker",
-    alias: 'widget.CategoryComboBox'
+    alias: 'widget.CategoryComboBox',
+
+    triggers: {
+        reload: {
+            cls: "x-form-reload-trigger",
+            weight: -1,
+            handler: function ()
+            {
+                this.store.load();
+            },
+            scope: 'this'
+        }
+    },
+
+    _oldValue: null,
+
+    initComponent: function () {
+		this.listenersStore = this.store.on({
+				scope: this,
+				// Workaround to remember the value when loading
+				beforeload: function () {
+                    this._oldValue = this.getValue();
+                },
+				// Set the old value when load is complete
+				load: function () {
+                    if (this._oldValue !== null) {
+                        this.setValue(this._oldValue);
+                    }
+                }
+    		});
+
+		this.callParent();
+    }
 });
