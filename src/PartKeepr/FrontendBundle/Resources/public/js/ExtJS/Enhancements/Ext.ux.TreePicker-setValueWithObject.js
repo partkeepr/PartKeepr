9a7ec8af4391a5d05ba9b7195d67ab2d7d11b28a
Felicitus <felicitus@felicitus.org>
2015-08-30 16:08:40 +0200

    Added implementation for Ext.ux.TreePicker to support setValue using a model, not by ID value

---
+++ src/PartKeepr/FrontendBundle/Resources/public/js/ExtJS/Enhancements/Ext.ux.TreePicker-setValueWithObject.js
@@ -0,0 +1,73 @@
+/**
+ * Enhancements for Ext.ux.TreePicker:
+ *
+ * - Allow setValue to be a model. If it's a model, select by the idProperty
+ * - Use | as separator for getPath/selectPath, because the default "/" separator doesn't work with JSON-LD IDs
+ */
+Ext.define("PartKeepr.ux.TreePicker", {
+    override: "Ext.ux.TreePicker",
+
+   /**
+     * Sets the specified value into the field
+     * @param {Mixed} value
+     * @return {Ext.ux.TreePicker} this
+     */
+    setValue: function(value) {
+        var me = this,
+            record;
+
+        me.value = value;
+
+        if (me.store.loading) {
+            // Called while the Store is loading. Ensure it is processed by the onLoad method.
+            return me;
+        }
+
+        // try to find a record in the store that matches the value
+        record = value ? me.store.getNodeById(value) : me.store.getRoot();
+        if (value === undefined) {
+            record = me.store.getRoot();
+            me.value = record.getId();
+        } else {
+            if (value.isModel) {
+                record = me.store.getNodeById(value.getId());
+            } else {
+                record = me.store.getNodeById(value);
+            }
+        }
+
+        // set the raw value to the record's display field if a record was found
+        me.setRawValue(record ? record.get(me.displayField) : '');
+
+        return me;
+    },
+    /**
+     * Runs when the picker is expanded.  Selects the appropriate tree node based on the value of the input element,
+     * and focuses the picker so that keyboard navigation will work.
+     * @private
+     */
+    onExpand: function()
+    {
+        var me = this,
+            picker = me.picker,
+            store = picker.store,
+            value = me.value,
+            node;
+
+        if (value) {
+            if (value.isModel) {
+                node = store.getNodeById(value.getId());
+            } else {
+                node = store.getNodeById(value);
+            }
+        }
+
+        if (!node) {
+            node = store.getRoot();
+        }
+
+        var path = node.getPath("@id", "|");
+
+        picker.selectPath(path, "@id", "|");
+    }
+});
