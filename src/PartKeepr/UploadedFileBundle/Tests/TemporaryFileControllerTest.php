267ec1e3472153e7bdfcc348186bd9010c94be74
Felicitus <felicitus@felicitus.org>
2015-09-13 16:53:23 +0200

    Moved tests to single file

--- src/PartKeepr/UploadedFileBundle/Tests/TemporaryFileControllerTest.php
+++ src/PartKeepr/UploadedFileBundle/Tests/TemporaryFileControllerTest.php
@@ -5,7 +5,7 @@ namespace PartKeepr\UploadedFileBundle\Tests;
 use Liip\FunctionalTestBundle\Test\WebTestCase;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
 
-class UploadTest extends WebTestCase
+class TemporaryFileControllerTest extends WebTestCase
 {
     public function testUploadAction()
     {
@@ -112,5 +112,60 @@ class UploadTest extends WebTestCase
 
         $this->assertObjectHasAttribute($attribute, $response);
         $this->assertEquals("Error",$response->$attribute);
+    }
+
+    public function testWebcamUploadAction()
+    {
+        $client = static::createClient();
+
+        $file = __DIR__."/Fixtures/files/uploadtest.png";
+        $mimeType = "image/png";
+        $extension = "png";
+
+        $contents = file_get_contents($file);
+
+        $fileString = "data:image/png;base64,".base64_encode($contents);
+
+        $client->request(
+            'POST',
+            '/api/temp_uploaded_files/webcamUpload',
+            array(),
+            array(),
+            array(),
+            $fileString
+        );
+
+        $response = json_decode($client->getResponse()->getContent());
+
+        $propertiesToCheck = array(
+            "@context",
+            "@id",
+            "@type",
+            "originalFilename",
+            "size",
+            "type",
+            "filename",
+            "mimeType",
+            "extension",
+            "description",
+            "legacyExtension",
+        );
+
+        foreach ($propertiesToCheck as $property) {
+            $this->assertObjectHasAttribute($property, $response);
+        }
+
+        $this->assertEquals(filesize($file), $response->size);
+        $this->assertEquals($mimeType, $response->mimeType);
+        $this->assertEquals("webcam.png", $response->originalFilename);
+        $this->assertEquals($extension, $response->extension);
+        $this->assertEquals("tempfile", $response->type);
+
+        $property = "@type";
+        $this->assertEquals("TempUploadedFile", $response->$property);
+    }
+
+    public function testGetFile () {
+
     }
 }
