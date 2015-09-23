5b8e0cd43bb5664617520c9ccde6a556eaaf87a4
Felicitus <felicitus@felicitus.org>
2015-09-23 16:47:56 +0200

    Don't use entity IDs for the URL when updating/deleting

--- src/PartKeepr/FrontendBundle/Resources/public/js/Data/store/UserPreferenceStore.js
+++ src/PartKeepr/FrontendBundle/Resources/public/js/Data/store/UserPreferenceStore.js
@@ -16,5 +16,11 @@ Ext.define('PartKeepr.data.store.UserPreferenceStore', {
      */
     model: "PartKeepr.AuthBundle.Entity.UserPreference",
 
-    pageSize: 99999999
+    pageSize: 99999999,
+
+    proxy: {
+        type: "Hydra",
+        url: '/api/user_preferences',
+        ignoreIds: true
+    }
 });
