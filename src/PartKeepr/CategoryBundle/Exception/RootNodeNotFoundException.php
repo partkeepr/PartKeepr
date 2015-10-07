93d50f1211e9169f1dfe5291ecd51bee3dce5e55
Felicitus <felicitus@felicitus.org>
2015-10-07 17:19:11 +0200

    Added message to the RootNodeNotFoundException

--- src/PartKeepr/CategoryBundle/Exception/RootNodeNotFoundException.php
+++ src/PartKeepr/CategoryBundle/Exception/RootNodeNotFoundException.php
@@ -4,5 +4,9 @@ namespace PartKeepr\CategoryBundle\Exception;
 
 class RootNodeNotFoundException extends \Exception
 {
-
+    public function __construct($message = "", $code = 0, \Exception $previous = null)
+    {
+        $message = "Root Category not found";
+        parent::__construct($message, $code, $previous);
+    }
 }
