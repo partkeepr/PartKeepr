a7765552a5c127868653f98b4b0bf28ed9516d81
Felicitus <felicitus@felicitus.org>
2015-10-12 14:43:26 +0200

    Set legacy flag when creating a proxy user

--- src/PartKeepr/AuthBundle/Services/UserService.php
+++ src/PartKeepr/AuthBundle/Services/UserService.php
@@ -167,7 +167,7 @@ class UserService
     private function createProxyUser($username, $provider)
     {
         $user = new User($username, $provider);
-q        $user->setLegacy(false);
+        $user->setLegacy(false);
         $this->entityManager->persist($user);
         $this->entityManager->flush();
 
