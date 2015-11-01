8a88f174bd0c475f7f960b7725d4a2faa38f8d06
Timo A. Hummel <felicitus@felicitus.org>
2015-11-01 22:01:32 +0100

    Removed legacy create user and set password commands, fos:user:create and fos:user:change-password are the proper commands. Fixes #449

--- src/PartKeepr/AuthBundle/PartKeeprAuthBundle.php
+++ src/PartKeepr/AuthBundle/PartKeeprAuthBundle.php
@@ -1,20 +1,8 @@
 <?php
-
 namespace PartKeepr\AuthBundle;
 
-use PartKeepr\AuthBundle\Console\Command\CreateUserCommand;
-use PartKeepr\AuthBundle\Console\Command\SetPasswordCommand;
-use Symfony\Component\Console\Application;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class PartKeeprAuthBundle extends Bundle
 {
-    /**
-     * {@inheritDoc}
-     */
-    public function registerCommands(Application $application)
-    {
-        $application->add(new CreateUserCommand());
-        $application->add(new SetPasswordCommand());
-    }
 }
