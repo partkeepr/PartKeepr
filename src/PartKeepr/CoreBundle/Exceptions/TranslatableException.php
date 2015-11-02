64a8129315f4af9b5fbbfcfbb1fd43ebdee2d516
Felicitus <felicitus@felicitus.org>
2015-11-02 16:51:02 +0100

    Use the method getMessageKey() to construct the exception message

--- src/PartKeepr/CoreBundle/Exceptions/TranslatableException.php
+++ src/PartKeepr/CoreBundle/Exceptions/TranslatableException.php
@@ -4,6 +4,10 @@ namespace PartKeepr\CoreBundle\Exceptions;
 
 abstract class TranslatableException extends \Exception
 {
+    public function __construct () {
+        parent::__construct($this->getMessageKey());
+    }
+
     /**
      * Returns the translatable message
      * @return mixed
