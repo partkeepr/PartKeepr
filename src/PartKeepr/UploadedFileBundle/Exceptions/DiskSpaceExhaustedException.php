136c72b91ba941d1294e8cf818eaa0a9f3100a92
Felicitus <felicitus@felicitus.org>
2015-11-02 16:51:20 +0100

    Throw an exception if the free disk space is exhausted

---
+++ src/PartKeepr/UploadedFileBundle/Exceptions/DiskSpaceExhaustedException.php
@@ -0,0 +1,12 @@
+<?php
+namespace PartKeepr\UploadedFileBundle\Exceptions;
+
+use PartKeepr\CoreBundle\Exceptions\TranslatableException;
+
+class DiskSpaceExhaustedException extends TranslatableException
+{
+    public function getMessageKey()
+    {
+        return "Not enough disk space or quota exhausted";
+    }
+}
