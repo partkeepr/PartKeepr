93aaaf8af9cddfb3770b302b2020a548d408cc02
Felicitus <felicitus@felicitus.org>
2015-11-05 17:39:40 +0100

    Use firstChild of the root node, as we are using a virtual root node

--- src/PartKeepr/FrontendBundle/Resources/public/js/ExtJS/Enhancements/Ext.ux.TreePicker-setValueWithObject.js
+++ src/PartKeepr/FrontendBundle/Resources/public/js/ExtJS/Enhancements/Ext.ux.TreePicker-setValueWithObject.js
@@ -26,7 +26,7 @@ Ext.define("PartKeepr.ux.TreePicker", {
         // try to find a record in the store that matches the value
         record = value ? me.store.getNodeById(value) : me.store.getRoot();
         if (value === undefined || value === null) {
-            record = me.store.getRoot();
+            record = me.store.getRoot().firstChild;
             me.value = record.getId();
         } else {
             if (value.isModel) {
