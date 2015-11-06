91f6126feb79813c019365713ac55d13ea446814
Timo A. Hummel <felicitus@felicitus.org>
2015-11-07 00:35:27 +0100

    Re-added missing base entity

---
+++ src/PartKeepr/CoreBundle/Entity/BaseEntity.php
@@ -0,0 +1,25 @@
+<?php
+namespace PartKeepr\CoreBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/** @ORM\MappedSuperclass */
+abstract class BaseEntity {
+	/**
+	* @ORM\Id
+	* @ORM\Column(type="integer")
+	* @ORM\GeneratedValue(strategy="AUTO")
+	* @var integer
+	*/
+	private $id;
+	
+	/**
+	 * Returns the ID of this object.
+	 * @param none
+	 * @return int The ID of this object
+	 */
+	public function getId () {
+		return $this->id;
+	}
+	
+}
