45b66cf7b9f11aaaa440df4ad230669029b2de6e
Timo A. Hummel <felicitus@felicitus.org>
2015-11-06 23:24:54 +0100

    Removed legacy config files, related to #459

--- config-test.php
+++
@@ -1,7 +0,0 @@
-<?php
-namespace PartKeepr;
-
-use PartKeepr\Util\Configuration;
-
-Configuration::setOption("partkeepr.database.dbname", "partkeepr-test");
-Configuration::setOption("partkeepr.database.driver", "pdo_sqlite");
