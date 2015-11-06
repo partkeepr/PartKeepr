9bfb281cd792ceae5dbb0e38489b0c77f1d773b3
Felicitus <felicitus@felicitus.org>
2015-11-06 19:05:36 +0100

    Added the grid exporter which exports the visible rows and columns in a grid and exports it to a specified format

---
+++ src/PartKeepr/FrontendBundle/Resources/public/js/Components/Exporter/GridExporter.js
@@ -0,0 +1,82 @@
+/**
+ * Exports a grid with all visible fields and rows
+ */
+Ext.define("PartKeepr.Exporter.GridExporter", {
+    constructor: function (gridPanel, format, extension)
+    {
+        this.gridPanel = gridPanel;
+        this.format = format;
+        this.extension = extension;
+    },
+    exportGrid: function ()
+    {
+        var columns = this.gridPanel.getColumns();
+
+        var store = this.gridPanel.getStore();
+        var records = store.getData();
+        var record, i, j, value, column, fieldValue;
+        var rows = [], rowValues = [], headers = [];
+
+        for (i = 0; i < columns.length; i++) {
+            if (!columns[i].isHidden()) {
+                rowValues.push(Ext.util.Format.stripTags(columns[i].text));
+            }
+        }
+
+        rows.push(rowValues);
+
+        for (i = 0; i < records.length; i++) {
+            rowValues = [];
+            record = records.getAt(i);
+
+            for (j = 0; j < columns.length; j++) {
+                column = columns[j];
+
+                fieldValue = record.data[column.dataIndex];
+
+                if (column.renderer && column.renderer.call) {
+                    value = column.renderer.call(
+                        column.usingDefaultRenderer ? column : column.scope || this.gridPanel,
+                        fieldValue,
+                        null,
+                        record,
+                        i,
+                        j,
+                        store,
+                        this.gridPanel.getView());
+
+                } else {
+                    value = fieldValue;
+                }
+
+                if (!column.isHidden()) {
+                    rowValues.push(Ext.util.Format.stripTags(value));
+                }
+            }
+
+            rows.push(rowValues);
+        }
+
+
+        var options = {
+            headers: {}
+        };
+
+        options.headers["Accept"] = "text/comma-separated-values";
+        options.jsonData = rows;
+        options.method = "POST";
+        //this.down("#formatSelector").getValue().get("mimetype");
+        options.url = PartKeepr.getBasePath() + "/api/export";
+        options.callback = Ext.bind(this.onExportSuccessful, this);
+        Ext.Ajax.request(options);
+    },
+    /**
+     * Callback for when the export is complete. Creates a client-side blob object and forces
+     * download of it.
+     */
+    onExportSuccessful: function (options, success, response)
+    {
+        var blob = new Blob([response.responseText], {type: this.format});
+        saveAs(blob, "export." + this.extension);
+    },
+});
---
+++ src/PartKeepr/FrontendBundle/Resources/public/js/Components/Exporter/GridExporterButton.js
@@ -0,0 +1,63 @@
+Ext.define("PartKeepr.Exporter.GridExporterButton", {
+    extend: "Ext.button.Button",
+
+    genericExporter: true,
+
+    initComponent: function ()
+    {
+        this.menu = [
+            {
+                text: i18n("Export Grid"),
+                menu: [
+                    {
+                        text: i18n("CSV"),
+                        handler: "onCSVExport",
+                        scope: this
+                    }, {
+                        text: i18n("Excel 2007 and later"),
+                        handler: "onExcelExport",
+                        scope: this
+                    }
+                ]
+            }
+        ];
+
+        console.log(this.genericExporter);
+        if (this.genericExporter) {
+            this.menu.push({
+                text: i18n("Custom Export…"),
+                handler: "onCustomExport",
+                scope: this
+            });
+        }
+        this.callParent(arguments);
+    },
+    onCSVExport: function ()
+    {
+        var gridPanel = this.up("gridpanel");
+        var exporter = Ext.create("PartKeepr.Exporter.GridExporter", gridPanel, "text/comma-separated-values", "csv");
+        exporter.exportGrid();
+    },
+    onExcelExport: function ()
+    {
+        var gridPanel = this.up("gridpanel");
+        var exporter = Ext.create("PartKeepr.Exporter.GridExporter", gridPanel,
+            "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "xlsx");
+        exporter.exportGrid();
+    },
+    onCustomExport: function () {
+        var j = Ext.create("Ext.window.Window", {
+            items: Ext.create("PartKeepr.Exporter.Exporter", {
+                model: this.up("gridpanel").getStore().getModel()
+            }),
+            title: i18n("Export"),
+            width: "80%",
+            height: "80%",
+            layout: 'fit',
+            maximizable: true,
+            closeAction: 'destroy'
+
+        });
+        j.show();
+    }
+});
