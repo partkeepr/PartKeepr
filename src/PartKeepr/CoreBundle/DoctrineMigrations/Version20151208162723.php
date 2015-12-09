e3af138d2ac110ef31074e725e6cb378cdb89309
Felicitus <felicitus@felicitus.org>
2015-12-09 17:41:11 +0100

    Added missing migration

---
+++ src/PartKeepr/CoreBundle/DoctrineMigrations/Version20151208162723.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace PartKeepr\CoreBundle\DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Sets all present users to be active
+ */
+class Version20151208162723 extends BaseMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        $this->performDatabaseUpgrade();
+        $activeUsersSQL = "UPDATE PartKeeprUser SET active = 1";
+        $this->addSql($activeUsersSQL);
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+
+    }
+}
