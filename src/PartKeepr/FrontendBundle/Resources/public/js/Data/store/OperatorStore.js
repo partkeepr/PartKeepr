aadde6bab78ef326d62568da52f5d4d0d82ae019
Felicia Hummel <felicia@partkeepr.com>
2017-01-08 16:16:40 +0100

    Added operator symbols and bool filters for numeric and string

--- src/PartKeepr/FrontendBundle/Resources/public/js/Data/store/OperatorStore.js
+++ src/PartKeepr/FrontendBundle/Resources/public/js/Data/store/OperatorStore.js
@@ -10,80 +10,142 @@ Ext.define("PartKeepr.Data.store.OperatorStore", {
     storeId: 'OperatorStore',
 
     fields: [
+        /**
+         * The operator to use for Ext.util.Filter
+         */
         {
+
             name: 'operator',
             type: 'string'
-        }, {
+        },
+        /**
+         * The symbol to display to the user
+         */
+        {
+            name: 'symbol',
+            type: 'string'
+        },
+        /**
+         * The description, so that the user knows what the operator does
+         */
+        {
             name: 'description',
             type: 'string'
         },
+        /**
+         * The operator type. May be "scalar" or "list"
+         */
         {
             name: 'type',
             type: 'string'
         },
+        /**
+         * Defines if the operator can be used for scalar comparisons
+         */
         {
             name: 'scalar',
             type: 'boolean'
         },
+        /**
+         * Defines if the operator can be used for entity comparisons
+         */
         {
             name: 'entity',
+            type: 'boolean'
+        },
+        /**
+         * Defines if the operator can be used for string values
+         */
+        {
+            name: 'string',
+            type: 'boolean'
+        },
+        /**
+         * Defines if the operator can be used for numeric values
+         */
+        {
+            name: 'numeric',
             type: 'boolean'
         }
     ],
     data: [
         {
             operator: "<",
+            symbol: "<",
             description: i18n("Less than"),
             type: 'scalar',
             scalar: true,
+            string: false,
+            numeric: true,
             entity: false
         },
         {
             operator: ">",
+            symbol: ">",
             description: i18n("Greater than"),
             type: 'scalar',
             scalar: true,
+            string: false,
+            numeric: true,
             entity: false
         },
         {
             operator: "=",
+            symbol: "=",
             description: i18n("Equals"),
             type: 'scalar',
             scalar: true,
+            string: true,
+            numeric: true,
             entity: true
         },
         {
             operator: ">=",
+            symbol: "≥",
             description: i18n("Greater than or equals"),
             type: 'scalar',
             scalar: true,
+            string: false,
+            numeric: true,
             entity: false
         },
         {
             operator: "<=",
+            symbol: "≤",
             description: i18n("Less than or equals"),
             type: 'scalar',
             scalar: true,
+            string: false,
+            numeric: true,
             entity: false
         },
         {
             operator: "!=",
+            symbol: "≠",
             description: i18n("Not equals"),
             type: 'scalar',
             scalar: true,
+            string: true,
+            numeric: true,
             entity: true
         }, {
             operator: "in",
+            symbol: "IN",
             description: i18n("Matches a list"),
             type: 'list',
             scalar: true,
+            string: true,
+            numeric: true,
             entity: true
         },
         {
             operator: "like",
+            symbol: "%%",
             description: i18n("Matches a subtext with wildcards (%)"),
             type: 'scalar',
             scalar: true,
+            string: true,
+            numeric: false,
             entity: false
         }
     ]
