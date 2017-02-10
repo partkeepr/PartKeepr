ffcbaed4cca213f1f709528b9e2c2d2f2a3e42a1
Felicia Hummel <felicia@partkeepr.com>
2017-02-10 10:36:51 +0100

    Check if the selected import column exists, related to #812

--- src/PartKeepr/ImportBundle/Configuration/FieldConfiguration.php
+++ src/PartKeepr/ImportBundle/Configuration/FieldConfiguration.php
@@ -79,6 +79,10 @@ class FieldConfiguration extends BaseConfiguration
                 return $this->fixedValue;
             break;
             case self::FIELDCONFIGURATION_COPYFROM:
+                if (!array_key_exists($this->copyFromField, $row)) {
+                    $this->log(sprintf("Error: Column %s for %s does not exist", $this->copyFromField, $this->fieldName));
+                    return null;
+                }
                 $this->log(sprintf("Would set field %s to value %s (import column %s)", $this->fieldName, $row[$this->copyFromField], $this->copyFromField));
 
                 return $row[$this->copyFromField];
