ecb39fef9c12a1b80e35114948882c9ac97a7ed3
Timo A. Hummel <felicitus@felicitus.org>
2018-04-28 18:34:47 +0200

    Fixed exception implementation for retrieving user preferences

--- src/PartKeepr/AuthBundle/Exceptions/UserPreferenceNotFoundException.php
+++ src/PartKeepr/AuthBundle/Exceptions/UserPreferenceNotFoundException.php
@@ -2,6 +2,7 @@
 
 namespace PartKeepr\AuthBundle\Exceptions;
 
+use PartKeepr\AuthBundle\Entity\User;
 use PartKeepr\CoreBundle\Exceptions\TranslatableException;
 
 /**
@@ -9,8 +10,22 @@ use PartKeepr\CoreBundle\Exceptions\TranslatableException;
  */
 class UserPreferenceNotFoundException extends TranslatableException
 {
+    private $user;
+    private $key;
+
+    public function __construct(User $user, $key, $code = 0, \Throwable $previous = null)
+    {
+        $this->user = $user;
+        $this->key = $key;
+
+        parent::__construct($code, $previous);
+    }
+
     public function getMessageKey()
     {
-        return 'The requested user preference was not found';
+        return sprintf('The requested user preference %s was not found for user %s',
+            $this->key,
+            $this->user->getUsername()
+        );
     }
 }
