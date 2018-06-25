e305b9ef9cae458496206199f40073a91ecf57c9
Felicia Hummel <felicia@drachenkatze.org>
2018-06-25 22:18:22 +0200

    Added more verbose error and corresponding wiki entry

--- web/setup/js/SetupTests/WebserverTest.js
+++ web/setup/js/SetupTests/WebserverTest.js
@@ -10,7 +10,7 @@ Ext.define('PartKeeprSetup.WebserverTest', {
         this.success = false;
         this.resultMessage = "Web Server misconfiguration";
         this.errors = ['Web Server Configuration error. Please read our <a href="https://wiki.partkeepr.org/wiki/KB00005:Web_Server_Configuration" target="_blank">wiki about the proper web server configuration</a>', response.responseText];
-
+        this.errors.push('Error 1: Tried to send a POST request to setup/webserverTest and it did not succeed.');
         if (this.callback) {
             this.callback.appendTestResult(this);
         }
