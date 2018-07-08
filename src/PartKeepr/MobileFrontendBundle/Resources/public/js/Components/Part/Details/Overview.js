0cc6859d2edbb989d0309dbf70fffad6b2a8f152
Felicia Hummel <felicia@drachenkatze.org>
2018-07-08 14:29:18 +0200

    Added remainding fields to the mobile part details

--- src/PartKeepr/MobileFrontendBundle/Resources/public/js/Components/Part/Details/Overview.js
+++ src/PartKeepr/MobileFrontendBundle/Resources/public/js/Components/Part/Details/Overview.js
@@ -9,10 +9,24 @@ Ext.define("PartKeepr.Components.Part.Details.Overview", {
         '<hr/>',
         '<p><span class="x-fa fa-folder"/> {categoryPath}</p>',
         '<hr/>',
-        '<p><span class="x-fa fa-database"/> Stock Level: {stockLevel} <span style="margin-left: 16px;" class="x-fa fa-arrow-circle-down"/> Minimum Stock Level {minStockLevel}</p>',
+        '<table><tr>',
+        '<td><span class="x-fa fa-database"/> Stock Level: {stockLevel}</td>',
+        '<td> <span style="margin-left: 16px;" class="x-fa fa-arrow-circle-down"/> Minimum Stock Level {minStockLevel}</td>',
+        '</tr><tr>',
+        '<td><span class="x-fa fa-archive"/> Storage Location: {storageLocation.name}</td>',
+        '<td> <span style="margin-left: 16px;" class="x-fa fa-info-circle"/> Footprint: {footprint.name}</td>',
+        '</tr><tr>',
+        '<td><span class="x-fa fa-info-circle"/> Status: {status}</td>',
+        '<td><span style="margin-left: 16px;" class="x-fa fa-info-circle"/> Condition: {condition}</td>',
+        '</tr><tr>',
+        '<td colspan="2"><span class="x-fa fa-info-circle"/> Internal Part Number: {internalPartNumber}</td>',
+        '</tr></table>',
+        '<hr/>',
+        '<p>{comment}</p>'
+
     ],
 
     setPart: function (part) {
-        this.setData(part.getData());
+        this.setData(part.getData(true));
     }
 });
