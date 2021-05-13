330f14ceef6ee2f2ab411d8593b29b7d30f8f423
daniel <daniel.calcoen@gmail.com>
2021-05-13 12:16:42 +0200

    phase 3 - operation to annotation - TempUploadedFile

--- src/PartKeepr/UploadedFileBundle/Entity/TempUploadedFile.php
+++ src/PartKeepr/UploadedFileBundle/Entity/TempUploadedFile.php
@@ -6,22 +6,67 @@ use ApiPlatform\Core\Annotation\ApiResource;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
 
+// ToDo: fix this - move this Route annotation to the correct place
+// "PartKeeprUploadedFileBundle:TemporaryFile:upload"         # Controller
+/**
+* @Route(
+*     name="TemporaryFileUpload",
+*     path="/temp_uploaded_files/upload",
+*     defaults={"_api_resource_class"=TempUploadedFile::class, "_api_collection_operation_name"="custom_post"}
+* )
+* @Method("POST")
+**/
+
+// ToDo: fix this - move this Route annotation to the correct place
+// "PartKeeprUploadedFileBundle:TemporaryFile:webcamUpload"         # Controller
+/**
+* @Route(
+*     name="TemporaryFileUploadWebcam",
+*     path="/temp_uploaded_files/webcamUpload",
+*     defaults={"_api_resource_class"=TempUploadedFile::class, "_api_collection_operation_name"="custom_post_webcam"}
+* )
+* @Method("POST")
+**/
+
+// ToDo: fix this - move this Route annotation to the correct place
+// "PartKeeprUploadedFileBundle:TemporaryFile:getFile"         # Controller
+/**
+* @Route(
+*     name="TemporaryFileGet",
+*     path="/temp_uploaded_files/{id}/getFile",
+*     defaults={"_api_resource_class"=TempUploadedFile::class, "_api_item_operation_name"="custom_get"}
+* )
+* @Method("GET")
+**/
+
+// ToDo: fix this - move this Route annotation to the correct place
+// "PartKeeprUploadedFileBundle:TemporaryFile:getMimeTypeIcon"         # Controller
+/**
+* @Route(
+*     name="TemporaryFileGetMimeTypeIcon",
+*     path="/temp_uploaded_files/{id}/getMimeTypeIcon",
+*     defaults={"_api_resource_class"=TempUploadedFile::class, "_api_item_operation_name"="custom_get_mimetype"}
+* )
+* @Method("GET")
+**/
+
 /**
  * Represents a temporary file. Temporary files are used when
  * a user uploaded a file, but not attached it to an entity.
  *
  * @ApiResource(
  *     collectionOperations={
- *       "post"={"method"="@resource.tempfile.collection_operation.custom_post"}
+ *       "post"={"method"="POST"},
+ *       "custom_post"={"route_name"="TemporaryFileUpload","hydra_context"={"title"="A custom operation","returns"="xmls:string"}},
+ *       "custom_post_webcam"={"route_name"="TemporaryFileUploadWebcam","hydra_context"={"title"="A custom operation","returns"="xmls:string"}}
  *     },
  *     itemOperations={
  *         "swagger"= {
  *          "method"="GET",
  *          },
- *         "get"={"method"="@resource.tempfile.item_operation.get"},
- *         "custom_get"={"method"="@resource.tempfile.item_operation.custom_get"},
- *         "custom_get_mimetype"={"method"="@resource.tempfile.item_operation.custom_get_mimetype"},
- *         "custom_post_webcam"={"method"="@resource.tempfile.collection_operation.custom_post_webcam"}
+ *         "get"={"method"="GET"},
+ *         "custom_get"={"route_name"="TemporaryFileGet","hydra_context"={"title"="A custom operation","returns"="xmls:string"}},
+ *         "custom_get_mimetype"={"route_name"="TemporaryFileGetMimeTypeIcon","hydra_context"={"title"="A custom operation","returns"="xmls:string"}}
  *     }
  * )
  * @ORM\Entity
