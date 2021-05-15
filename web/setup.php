5b82809ac0f67208c460ed86d5384d85447692b3
daniel <daniel.calcoen@gmail.com>
2021-05-16 01:27:53 +0200

    change boostrap by autoload for load kernel at setup test

--- web/setup.php
+++ web/setup.php
@@ -2,10 +2,11 @@
 
 use Symfony\Component\HttpFoundation\Request;
 
-$loader = require_once __DIR__.'/../var/bootstrap.php.cache';
+// $loader = require_once  __DIR__ . "/../var/bootstrap.php.cache";
+$loader = require_once __DIR__.'/../vendor/autoload.php';
 
-require_once __DIR__.'/../app/SetupAppKernel.php';
-require_once __DIR__.'/../app/AppKernel.php';
+require_once  __DIR__ . "/../app/SetupAppKernel.php";
+require_once  __DIR__ . "/../app/AppKernel.php";
 
 $kernel = new SetupAppKernel('setup', true);
 $kernel->loadClassCache();
