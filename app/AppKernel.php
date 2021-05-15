3db087d1d3c9943bf7e7f5450d9342568bc92c7d
daniel <daniel.calcoen@gmail.com>
2021-05-15 13:48:11 +0200

    restore Sensio GeneratorBundle in AppKernel

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -38,7 +38,7 @@ class AppKernel extends Kernel
             $bundles[] = new Symfony\Bundle\DebugBundle\DebugBundle();
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
-//          $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
+            $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
             $bundles[] = new Liip\FunctionalTestBundle\LiipFunctionalTestBundle();
         }
 
