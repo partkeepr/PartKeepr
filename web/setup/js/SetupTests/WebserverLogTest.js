072679af96e1ad5f0ea694b3b3b946e147ac922f
daniel <daniel.calcoen@gmail.com>
2021-05-16 09:17:33 +0200

    some missing app/logs changed to app/logs

--- web/setup/js/SetupTests/WebserverLogTest.js
+++ web/setup/js/SetupTests/WebserverLogTest.js
@@ -3,10 +3,10 @@
  */
 Ext.define('PartKeeprSetup.WebserverLogDirectoryTest', {
     extend: 'PartKeeprSetup.AbstractTest',
-    url: '../../app/logs/ignore.json',
+    url: '../../var/logs/ignore.json',
     method: 'GET',
     name: "PHP",
-    message: "app/logs access check",
+    message: "var/logs access check",
     onSuccess: function (response) {
         try {
             var responseObj = Ext.decode(response.responseText);
@@ -17,7 +17,7 @@ Ext.define('PartKeeprSetup.WebserverLogDirectoryTest', {
         if (responseObj.message && responseObj.message === "readable") {
             this.success = false;
             this.resultMessage = "Web Server misconfiguration";
-            this.errors = ['Your <code>app/logs</code> directory is readable. Please either move your web server\'s document root to the web/ directory or configure <code>app/logs</code> to disallow access. For further information please read <a href="https://wiki.partkeepr.org/wiki/KB00007:Prevent_Access_to_the_logs_directory" target="_blank">wiki about how to prevent access to the logs directory</a>'];
+            this.errors = ['Your <code>var/logs</code> directory is readable. Please either move your web server\'s document root to the web/ directory or configure <code>var/logs</code> to disallow access. For further information please read <a href="https://wiki.partkeepr.org/wiki/KB00007:Prevent_Access_to_the_logs_directory" target="_blank">wiki about how to prevent access to the logs directory</a>'];
 
             if (this.callback) {
                 this.callback.appendTestResult(this);
@@ -36,7 +36,7 @@ Ext.define('PartKeeprSetup.WebserverLogDirectoryTest', {
      */
     onFailure: function () {
         this.success = true;
-        this.resultMessage = "app/logs not readable";
+        this.resultMessage = "var/logs not readable";
 
         if (this.callback) {
             this.callback.appendTestResult(this);
