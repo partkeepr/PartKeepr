f7c11dd80218f990e8d43fb97fee243a6609e220
benfre <benfre@users.noreply.github.com>
2022-11-28 09:56:53 +0800

    fix translation uniqueness and case sensitive. add Chinese translation zh-hans.

--- generate_translate_template.ps1
+++ generate_translate_template.ps1
@@ -1,9 +1,9 @@
 
-$case1 = $(Get-ChildItem -r -i *.js | Select-String "i18n\(`"([^`"]*?)`"\)" -AllMatches | ForEach-Object -Process {$_.Matches[0].Groups[1].Value} | Select-Object -Unique) 
-$case2 = $(Get-ChildItem -r -i *.js | Select-String "i18n\(`'([^`']*?)`'\)" -AllMatches | ForEach-Object -Process {$_.Matches[0].Groups[1].Value} | Select-Object -Unique)
+$case1 = $(Get-ChildItem -r -i *.js | Select-String "i18n\(`"([^`"]*?)`"\)" -AllMatches | ForEach-Object -Process {$_.Matches[0].Groups[1].Value}) 
+$case2 = $(Get-ChildItem -r -i *.js | Select-String "i18n\(`'([^`']*?)`'\)" -AllMatches | ForEach-Object -Process {$_.Matches[0].Groups[1].Value})
 $all_case = $case1 + $case2
 
-$key_pairs = $(echo $all_case |Sort-Object |ForEach-Object -Process {$x = $_ -replace "`"","\`""; "`n`"{0}`":`"{0}`"" -f $x})
+$key_pairs = $( $all_case |Sort-Object -Unique -CaseSensitive|ForEach-Object -Process {$x = $_ -replace "`"","\`""; "`n`"{0}`":`"{0}`"" -f $x})
 
 $all_str = $key_pairs -join ','
 
